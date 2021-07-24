7c78dcdcc7ee7f9a1d83a11affaf0589b54187e0
jinsilna <zxcv392766!>
2021-07-24 15:36:48 +0900

    계산기 ( C , + , - , = ) 기능을 구현

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,79 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var display: UILabel!
+    
+    var isPlus = false;
+    var isMinus = false;
+    
+    var sum = Int();
+    var num = Int();
+    var userTyping = false;
+    @IBAction func touch1(_ sender: UIButton) {
+        
+        // func clear
+        if( display.text == "0"){
+            display.text = ""
+        }
+        
+        let dight = sender.currentTitle!
+        print("thouched \(dight) dight");
+        if userTyping {
+            let textCurrentDisplay = display.text!
+            display.text = textCurrentDisplay + dight
+        }else{
+            display.text = dight
+        }
+        userTyping = true;
     }
-
-
+    // C 버튼 기능
+    @IBAction func clear(_ sender: Any) {
+        display.text = "0"
+        sum = 0;
+        num = 0;
+    }
+    
+    @IBAction func result(_ sender: Any) {
+        if isPlus{
+            sum  = sum + Int(display.text!)!
+            print("result Plus", sum)
+            isPlus = false
+            display.text = String(sum)
+        }
+        
+        if isMinus {
+            sum  = sum - Int(display.text!)!
+            print("result Minus", sum)
+            isMinus = false
+            display.text = String(sum)
+        }
+    }
+    
+    
+    @IBAction func plus(_ sender: Any) {
+        if(isPlus==false){
+            num = Int(display.text!)!
+            print("num",num)
+            sum = num + sum
+            print("sum",sum)
+            userTyping = false
+            isPlus = true
+        }
+    }
+    
+    @IBAction func minus(_ sender: Any) {
+        if(isMinus==false){
+            num = Int(display.text!)!
+            print("num",num)
+            sum = num - sum
+            print("sum",sum)
+            userTyping = false
+            isMinus = true
+        }
+    }
+    
+    
 }
 
